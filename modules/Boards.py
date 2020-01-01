efd3fc4dc80dac84fae360e40019ae0005e1f292
Varun Somani <varunsomani95@gmail.com>
2020-01-01 22:53:30 +0530

    Added Boards didn't add last time

---
+++ modules/Boards.py
@@ -0,0 +1,132 @@
+from DeckOfCards import Card,Deck
+row1 = [
+        Card('Free',0),
+        Card('Diamonds',6),
+        Card('Diamonds',7),
+        Card('Diamonds',8),
+        Card('Diamonds',9),
+        Card('Diamonds',10),
+        Card('Diamonds',12),
+        Card('Diamonds',13),
+        Card('Diamonds',1),
+        Card('Free',0)
+]
+row2 =  [
+        Card('Diamonds',5),
+        Card('Hearts',3),
+        Card('Hearts',2),
+        Card('Spades',2),
+        Card('Spades',3),
+        Card('Spades',4),
+        Card('Spades',5),
+        Card('Spades',6),
+        Card('Spades',7),
+        Card('Clubs',1)
+]
+row3 =  [
+        Card('Diamonds',4),
+        Card('Hearts',4),
+        Card('Diamonds',13),
+        Card('Diamonds',1),
+        Card('Clubs',1),
+        Card('Clubs',13),
+        Card('Clubs',12),
+        Card('Clubs',10),
+        Card('Spades',8),
+        Card('Clubs',13)
+]
+row4 =  [
+        Card('Diamonds',3),
+        Card('Hearts',5),
+        Card('Diamonds',12),
+        Card('Hearts',12),
+        Card('Hearts',10),
+        Card('Hearts',9),
+        Card('Hearts',8),
+        Card('Clubs',9),
+        Card('Spades',9),
+        Card('Clubs',12)
+]
+row5 =  [
+        Card('Diamonds',2),
+        Card('Hearts',6),
+        Card('Diamonds',10),
+        Card('Hearts',13),
+        Card('Hearts',3),
+        Card('Hearts',2),
+        Card('Hearts',7),
+        Card('Clubs',8),
+        Card('Spades',10),
+        Card('Clubs',10)
+]
+row6 =  [
+        Card('Spades',1),
+        Card('Hearts',7),
+        Card('Diamonds',9),
+        Card('Hearts',1),
+        Card('Hearts',4),
+        Card('Hearts',5),
+        Card('Hearts',6),
+        Card('Clubs',7),
+        Card('Spades',12),
+        Card('Clubs',9)
+]
+row7 =  [
+        Card('Spades',13),
+        Card('Hearts',8),
+        Card('Diamonds',8),
+        Card('Clubs',2),
+        Card('Clubs',3),
+        Card('Clubs',4),
+        Card('Clubs',5),
+        Card('Clubs',6),
+        Card('Spades',13),
+        Card('Clubs',8)
+]
+row8 =  [
+        Card('Spades',12),
+        Card('Hearts',9),
+        Card('Diamonds',7),
+        Card('Diamonds',6),
+        Card('Diamonds',5),
+        Card('Diamonds',4),
+        Card('Diamonds',3),
+        Card('Diamonds',2),
+        Card('Spades',1),
+        Card('Clubs',7)
+]
+row9 =  [
+        Card('Spades',10),
+        Card('Hearts',10),
+        Card('Hearts',12),
+        Card('Hearts',13),
+        Card('Hearts',1),
+        Card('Clubs',2),
+        Card('Clubs',3),
+        Card('Clubs',4),
+        Card('Clubs',5),
+        Card('Clubs',6)
+]                                        
+row10 =  [
+        Card('Free',0),
+        Card('Spades',9),
+        Card('Spades',8),
+        Card('Spades',7),
+        Card('Spades',6),
+        Card('Spades',5),
+        Card('Spades',4),
+        Card('Spades',3),
+        Card('Spades',2),
+        Card('Free',0)
+]
+defaultBoard = [row1,
+                row2,
+                row3,
+                row4,
+                row5,
+                row6,
+                row7,
+                row8,
+                row9,
+                row10
+                ]       
