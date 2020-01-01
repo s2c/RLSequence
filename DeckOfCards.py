534900fae4a0c56a183403dd2095b43ded258112
Varun Somani <varunsomani95@gmail.com>
2020-01-01 13:50:05 +0530

    Finished cards

--- DeckOfCards.py
+++ DeckOfCards.py
@@ -1,1 +1,31 @@
-# import
+import random
+
+class Card(object):
+    """Holds each individual Card"""
+    def __init__(self, suit=None,rank=None):
+        super(Card, self).__init__()
+        self.suit = suit
+        self.rank = rank
+    def show(self):
+        return(self.rank,self.suit)
+
+class Deck(object):
+    """Holds card objects"""
+    def __init__(self,deckSize=52):
+        super(Deck, self).__init__()
+        self.cards = [Card]*deckSize
+        self.build()
+
+    def build(self):
+        suits = ['Diamonds','Hearts','Clubs','Spades']
+        ranks = [x for x in range(1,14)]
+        for i,s in enumerate(suits):
+            for j,r in enumerate(ranks):
+                self.cards[i*13 + j] = Card(s,r)
+    def show(self):
+        showDeck = [x.show() for x in self.cards]
+        return showDeck
+    def shuffle(self):
+        self.cards = random.shuffle(self.cards)
+    def drawCard(self):
+        return self.cards.pop()
